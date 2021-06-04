8bb495776f7840268b0f1e5d624e095d3f1c4a1d
avira <aviramshovali@gmail.com>
2021-06-04 18:58:27 +0300

    Added basic

--- server.c
+++ server.c
@@ -6,6 +6,7 @@
 #define SCHED_ALG_RANDOM 3
 
 
+
 /*
  * ***********************************
  *          Queue implementaion
@@ -86,8 +87,8 @@ void destroyQueue(Queue* q) {
 void getargs(int argc, char *argv[], int *port, int *num_of_threads, int *queue_size, int *schedalg)
 {
     if (argc < 5) {
-	fprintf(stderr, "Usage: %s [portnum] [threads] [queue_size] [schedalg]\n", argv[0]);
-	exit(1);
+        fprintf(stderr, "Usage: %s [portnum] [threads] [queue_size] [schedalg]\n", argv[0]);
+        exit(1);
     }
     *port = atoi(argv[1]);
     *num_of_threads = atoi(argv[2]);
@@ -106,19 +107,33 @@ void getargs(int argc, char *argv[], int *port, int *num_of_threads, int *queue_
         *schedalg = SCHED_ALG_RANDOM;
     }
 }
+// some global variables
+pthread_cond_t empty_g, full_g;
+pthread_mutex_t global_lock;
+Queue* requests_queue;
 
+void* handleRequests(void* thread_id /* NOTE: this will be used mainly for section C when we want to gather thread statistics */) {
 
+    // NOTE: this is not busy waiting!!
+    //      there is usage in condition variables in dequeue
+    while(1) {
+        int fd = dequeue(requests_queue);
+        // TODO: add a list (or maybe queue) for requests currently being processed (Page 6 Hint No.1 in manual)
+        requestHandle(fd);
+        Close(fd);
+    }
+
+}
 int main(int argc, char *argv[])
 {
     int listenfd, connfd, port, clientlen, num_of_threads, queue_size, schedalg;
     struct sockaddr_in clientaddr;
 
     getargs(argc, argv, &port, &num_of_threads, &queue_size, &schedalg);
-    pthread_cond_t empty, full;
 
     //initializing condition variables --> this will allways succeed
-    pthread_cond_init( &empty, NULL);
-    pthread_cond_init( &full, NULL);
+    pthread_cond_init( &empty_g, NULL);
+    pthread_cond_init( &full_g, NULL);
 
 
     // 
@@ -126,23 +141,22 @@ int main(int argc, char *argv[])
     //
     pthread_t worker_thread[num_of_threads];
     for (int i = 0; i < num_of_threads; ++i) {
-        pthread_create(worker_thread[i],NULL, /* todo some function maybe deequeue? */ , i /* todo maybe change this? */ );
+        pthread_create(worker_thread[i],NULL,handleRequests , &i );
     }
+    requests_queue = initQueue(queue_size, &empty_g, &full_g, &global_lock);
 
     listenfd = Open_listenfd(port);
     while (1) {
-	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
 
-	// 
-	// HW3: In general, don't handle the request in the main thread.
-	// Save the relevant info in a buffer and have one of the worker threads 
-	// do the work. 
-	// 
-	requestHandle(connfd);
+        //
+        // HW3: In general, don't handle the request in the main thread.
+        // Save the relevant info in a buffer and have one of the worker threads
+        // do the work.
+        //
+        enqueue(requests_queue, connfd);
 
-
-	Close(connfd);
     }
 
 }
